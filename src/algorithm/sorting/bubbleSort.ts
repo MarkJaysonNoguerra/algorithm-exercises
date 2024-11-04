9612afa90915d079747b688e197e123a2459cd99
mark <noguerra9999@gmail.com>
2024-11-05 07:03:11 +0800

    remove the changes to bubblesort to pratice it next time

--- src/algorithm/sorting/bubbleSort.ts
+++ src/algorithm/sorting/bubbleSort.ts
@@ -1,14 +1,4 @@
 // O(n squared)
 export const bubbleSort = (input: number[]): number[] => {
-  for (let i = 0; i < input.length; i++) {
-    for (let j = 0; j < input.length - 1 - i; j++) {
-      if (input[j] > input[j + 1]) {
-        const temp = input[j];
-        input[j] = input[j + 1];
-        input[j + 1] = temp;
-      }
-    }
-  }
-
-  return input;
+    return input;
 };
