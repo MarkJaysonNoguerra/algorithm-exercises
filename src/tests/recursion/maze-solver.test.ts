4ac7f295de501a4b3efd24f96a79a1bf7ddb4061
mark <noguerra9999@gmail.com>
2024-11-12 07:24:39 +0800

    added recursion practice using simple maze solver

---
+++ src/tests/recursion/maze-solver.test.ts
@@ -0,0 +1,28 @@
+import { describe, expect, test } from "vitest";
+import { mazeSolver } from "../../recursion/maze-solver";
+
+describe("Test for maze-solver", () => {
+  test("simple maze test", () => {
+    const maze = [
+      ["#", "", "#", "#", "", "#"],
+      ["#", "", "", "#", "", "#"],
+      ["#", "", "", "#", "", "#"],
+      ["", "", "#", "#", "", "#"],
+      ["", "", "", "", "", "#"],
+      ["#", "", "", "#", "#", "#"],
+    ];
+
+    const solvedPath = mazeSolver(maze, "#", { x: 4, y: 0 }, { x: 1, y: 5 });
+    expect(solvedPath).toStrictEqual([
+      { x: 4, y: 0 },
+      { x: 4, y: 1 },
+      { x: 4, y: 2 },
+      { x: 4, y: 3 },
+      { x: 4, y: 4 },
+      { x: 3, y: 4 },
+      { x: 2, y: 4 },
+      { x: 1, y: 4 },
+      { x: 1, y: 5 },
+    ]);
+  });
+});
