05f6ef6abf7b2e1889fe81b52c52a97ba04c6b29
mark <noguerra9999@gmail.com>
2024-11-14 07:02:57 +0800

    remove implementation for maze-solver to test myself next time

--- src/recursion/maze-solver.ts
+++ src/recursion/maze-solver.ts
@@ -3,65 +3,6 @@ type Point = {
   y: number;
 };
 
-const dirs = [
-  { x: 0, y: -1 },
-  { x: 1, y: 0 },
-  { x: 0, y: 1 },
-  { x: -1, y: 0 },
-];
-
-const walk = (
-  maze: string[][],
-  wall: string,
-  currentPoint: Point,
-  goal: Point,
-  paths: Point[],
-  seen: boolean[][]
-): boolean => {
-  // is the edge
-  if (
-    currentPoint.x < 0 ||
-    currentPoint.y < 0 ||
-    currentPoint.x >= maze[0].length ||
-    currentPoint.y >= maze[0].length
-  ) {
-    return false;
-  }
-
-  // is wall
-  if (maze[currentPoint.y][currentPoint.x] === wall) {
-    return false;
-  }
-
-  // if already seen
-  if (seen[currentPoint.y][currentPoint.x]) {
-    return false;
-  }
-
-  // is the goal
-  if (currentPoint.x === goal.x && currentPoint.y === goal.y) {
-    paths.push(currentPoint);
-    return true;
-  }
-
-  paths.push(currentPoint);
-  seen[currentPoint.y][currentPoint.x] = true;
-
-  for (const dir of dirs) {
-    const newPoint = {
-      x: currentPoint.x + dir.x,
-      y: currentPoint.y + dir.y,
-    } as Point;
-
-    if (walk(maze, wall, newPoint, goal, paths, seen)) {
-      return true;
-    }
-  }
-
-  paths.pop();
-  return false;
-};
-
 export const mazeSolver = (
   maze: string[][],
   wall: string,
@@ -69,11 +10,5 @@ export const mazeSolver = (
   endPoint: Point
 ): Point[] => {
   const solvePath: Point[] = [];
-  const seen = Array.from({ length: maze[0].length }, () => {
-    return Array.from({ length: maze[0].length }, () => false);
-  });
-
-  walk(maze, wall, startPoint, endPoint, solvePath, seen);
-
   return solvePath;
 };
