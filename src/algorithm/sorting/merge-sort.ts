793bccda2b8462ee3a25e124b1b09c7cc5f01d08
mark <noguerra9999@gmail.com>
2024-11-22 07:57:35 +0800

    Undo the merge sort implementation to reinforce learning next time

--- src/algorithm/sorting/merge-sort.ts
+++ src/algorithm/sorting/merge-sort.ts
@@ -1,35 +1,7 @@
-const merge = (left: number[], right: number[]): number[] => {
-  let leftIndex = 0;
-  let rightIndex = 0;
-  const result = [];
-
-  while (leftIndex < left.length && rightIndex < right.length) {
-    if (left[leftIndex] < right[rightIndex]) {
-      result.push(left[leftIndex]);
-      leftIndex++;
-    } else {
-      result.push(right[rightIndex]);
-      rightIndex++;
-    }
-  }
-
-  for (; leftIndex < left.length; leftIndex++) {
-    result.push(left[leftIndex]);
-  }
-  for (; rightIndex < right.length; rightIndex++) {
-    result.push(right[rightIndex]);
-  }
-
-  return result;
+const merge = (): number[] => {
+  return [];
 };
 
 export const mergeSort = (input: number[]): number[] => {
-  if (input.length === 1) {
-    return input;
-  }
-
-  const middle = Math.floor(input.length / 2);
-  const left = input.slice(0, middle);
-  const right = input.slice(middle);
-  return merge(mergeSort(left), mergeSort(right));
+  return input;
 };
