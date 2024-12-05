8807e6dcbd0768faa019e46a8a27d479136cc13d
mark jayson noguerra <noguerra9999@gmail.com>
2024-12-06 06:20:31 +0800

    undo implementation for doubly-linked-list to be reimplement for reinforcing learning

--- src/data-structure/doubly-linked-list.ts
+++ src/data-structure/doubly-linked-list.ts
@@ -14,171 +14,28 @@ export class DoublyLinkedList<T> {
 
 
   append(value: T): this {
-    const node = {
-      value,
-    } as Node<T>
-
-    this.length++;
-    if (!this.head || !this.tail) {
-      this.tail = this.head = node;
-    } else {
-      this.tail.next = node;
-      node.previous = this.tail;
-      this.tail = node;
-    }
-
     return this
   }
 
   prepend(value: T): this {
-    const node = {
-      value
-    } as Node<T>;
-
-    this.length++;
-    if (!this.head || !this.tail) {
-      this.head = this.tail = node;
-    } else {
-      node.next = this.head;
-      this.head.previous = node;
-      this.head = node;
-    }
 
     return this
   }
 
   insertAt(value: T, index: number): this {
-    if ((index > 0 && index > this.length - 1) || index < 0) {
-      throw Error("out of range index");
-    }
-
-    if (index === 0) {
-      this.prepend(value);
-    } else if (index === this.length - 1) {
-      this.append(value);
-    } else {
-      const currentNode = this.getAt(index);
-      const newNode = {
-        value
-      } as Node<T>
-
-      newNode.next = currentNode;
-      newNode.previous = currentNode.previous;
-      currentNode.previous = newNode;
-
-      newNode.previous!.next = newNode;
-      this.length++;
-    }
-
     return this
   }
 
   remove(value: T): number | undefined {
-    if (this.head?.value === value) {
-      if (this.head.next) {
-        this.head = this.head.next;
-        this.head.previous = undefined;
-      } else {
-        this.head = this.tail = undefined;
-      }
-
-      this.length--;
-      return 0;
-    }
-
-    if (this.tail?.value === value) {
-      if (this.tail.previous) {
-        this.tail = this.tail.previous;
-        this.tail.next = undefined;
-      } else {
-        this.head = this.tail = undefined;
-      }
-
-      this.length--;
-      return this.length;
-    }
-
-    let currentNode = this.head;
-    let currentIndex = 0;
-    while (currentNode?.value) {
-      if (currentNode.value === value) {
-        this.length--;
-
-        const left = currentNode.previous;
-        if (left) {
-          left.next = currentNode.next;
-        }
-
-        const right = currentNode.next;
-        if (right) {
-          right.previous = currentNode.previous;
-        }
-
-        return currentIndex;
-      }
-
-      currentNode = currentNode.next;
-      currentIndex++;
-    }
-
     return undefined;
   }
 
   removeAt(index: number): T | undefined {
-    if (index > this.length) {
-      throw Error("out of range index");
-    }
-
-    if (index === 0) {
-      this.length--;
-      const value = this.head?.value;
-      this.head = this.head?.next;
-      if (this.head) {
-        this.head.previous = undefined;
-      }
-
-      return value;
-    }
-
-    if (index === this.length - 1) {
-      this.length--;
-      const value = this.tail?.value;
-      this.tail = this.tail?.previous;
-      if (this.tail) {
-        this.tail.next = undefined;
-      }
-
-      return value;
-    }
-
-
-    const node = this.getAt(index);
-
-    this.length--;
-    const left = node.previous;
-    if (left) {
-      left.next = node.next;
-    }
-
-    const right = node.next;
-    if (right) {
-      right.previous = node.previous;
-    }
-
-    return node.value;
+    return undefined;
   }
 
   getContents(): T[] {
-
-    let current = this.head;
-    const result = [];
-
-    while (current?.value) {
-      result.push(current.value);
-      current = current.next;
-    }
-
-    return result;
+    return [];
   }
 
   getSize(): number {
@@ -186,19 +43,5 @@ export class DoublyLinkedList<T> {
   }
 
   private getAt(index: number): Node<T> {
-
-    let currentNode = this.head;
-    let currentIndex = 0;
-
-    while (currentNode?.value) {
-      if (currentIndex === index) {
-        return currentNode;
-      }
-
-      currentNode = currentNode.next;
-      currentIndex++;
-    }
-
-    throw Error("out of range index");
   }
 }
