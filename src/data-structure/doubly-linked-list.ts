1a11ef6128da413b4e45f727acfb2492a759b4cb
mark jayson noguerra <noguerra9999@gmail.com>
2024-12-05 08:00:14 +0800

    in progress doubly linked list implementation

--- src/data-structure/doubly-linked-list.ts
+++ src/data-structure/doubly-linked-list.ts
@@ -1,33 +1,165 @@
+type Node<T> = {
+  previous?: Node<T>;
+  next?: Node<T>;
+  value: T
+}
+
 export class DoublyLinkedList<T> {
-  prepend(item: T): this {
+
+  private head?: Node<T>
+
+  private tail?: Node<T>;
+
+  private length = 0;
+
+
+  append(value: T): this {
+    const node = {
+      value,
+    } as Node<T>
+
+    this.length++;
+    if (!this.head || !this.tail) {
+      this.tail = this.head = node;
+    } else {
+      this.tail.next = node;
+      node.previous = this.tail;
+      this.tail = node;
+    }
+
     return this
   }
 
-  append(item: T): this {
+  prepend(value: T): this {
+    const node = {
+      value
+    } as Node<T>;
+
+    this.length++;
+    if (!this.head || !this.tail) {
+      this.head = this.tail = node;
+    } else {
+      node.next = this.head;
+      this.head.previous = node;
+      this.head = node;
+    }
+
     return this
   }
 
-  insertAt(item: T, index: number): this {
+  insertAt(value: T, index: number): this {
+    if ((index > 0 && index > this.length - 1) || index < 0) {
+      throw Error("out of range index");
+    }
+
+    if (index === 0) {
+      this.prepend(value);
+    } else if (index === this.length - 1) {
+      this.append(value);
+    } else {
+      const currentNode = this.getAt(index);
+      const newNode = {
+        value
+      } as Node<T>
+
+      newNode.next = currentNode;
+      newNode.previous = currentNode.previous;
+      currentNode.previous = newNode;
+
+      newNode.previous!.next = newNode;
+      this.length++;
+    }
+
     return this
   }
 
-  remove(item: T): this {
-    return this
+  remove(value: T): number | undefined {
+    if (this.head?.value === value) {
+      if (this.head.next) {
+        this.head = this.head.next;
+        this.head.previous = undefined;
+      } else {
+        this.head = this.tail = undefined;
+      }
+
+      this.length--;
+      return 0;
+    }
+
+    if (this.tail?.value === value) {
+      if (this.tail.previous) {
+        this.tail = this.tail.previous;
+        this.tail.next = undefined;
+      } else {
+        this.head = this.tail = undefined;
+      }
+
+      this.length--;
+      return this.length;
+    }
+    
+    let currentNode = this.head;
+    let currentIndex = 0;
+    while(currentNode?.value) {
+      if (currentNode.value === value) {
+        this.length--;
+
+        const left = currentNode.previous;
+        if (left) {
+          left.next = currentNode.next;
+        }
+
+        const right = currentNode.next;
+        if (right) {
+          right.previous = currentNode.previous;
+        }
+
+        return currentIndex;
+      }
+
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+    
+    return undefined;
   }
 
   removeAt(index: number): this {
     return this
   }
 
-  get(index: number): T | undefined {
-    return undefined;
+  private getAt(index: number): Node<T> {
+
+    let currentNode = this.head;
+    let currentIndex = 0;
+
+    while (currentNode?.value) {
+      if (currentIndex === index) {
+        return currentNode;
+      }
+
+      currentNode = currentNode.next;
+      currentIndex++;
+    }
+
+
+    throw Error("out of range index");
   }
 
-  getContents(): number[] {
-    return []
+  getContents(): T[] {
+
+    let current = this.head;
+    const result = [];
+
+    while (current?.value) {
+      result.push(current.value);
+      current = current.next;
+    }
+
+    return result;
   }
 
   getSize(): number {
-    return 0
+    return this.length;
   }
 }
--- src/tests/data-structure/doubly-linked-list.test.ts
+++ src/tests/data-structure/doubly-linked-list.test.ts
@@ -56,7 +56,7 @@ describe("Test for doubly linked list", () => {
     ]);
   });
 
-  test("remove method", () => {
+  test("insertAt method at first", () => {
     const linkedList = new DoublyLinkedList<number>();
     linkedList
       .prepend(123)
@@ -68,27 +68,136 @@ describe("Test for doubly linked list", () => {
       .prepend(3)
       .prepend(4)
       .prepend(65)
-      .remove(1);
+      .insertAt(50, 0);
 
-    expect(linkedList.getSize()).toBe(9);
+    expect(linkedList.getSize()).toBe(10);
+    expect(linkedList.getContents()).toStrictEqual([
+      50, 65, 4, 3, 2, 9, 1323, 233, 1, 123,
+    ]);
+  });
+
+
+  test("insertAt method at the end", () => {
+    const linkedList = new DoublyLinkedList<number>();
+    linkedList
+      .prepend(123)
+      .prepend(1)
+      .prepend(233)
+      .prepend(1323)
+      .prepend(9)
+      .prepend(2)
+      .prepend(3)
+      .prepend(4)
+      .prepend(65)
+      .insertAt(50, 8);
+
+    expect(linkedList.getSize()).toBe(10);
+    expect(linkedList.getContents()).toStrictEqual([
+      65, 4, 3, 2, 9, 1323, 233, 1, 123, 50
+    ]);
+  });
+
+  test("insert method first", () => {
+    const linkedList = new DoublyLinkedList<number>();
+    linkedList
+      .insertAt(50, 0);
+
+    expect(linkedList.getSize()).toBe(1);
+    expect(linkedList.getContents()).toStrictEqual([50]);
+  });
+
+  test("insert method out of range", () => {
+    const linkedList = new DoublyLinkedList<number>();
+    linkedList
+      .insertAt(50, 0);
+
+    expect(() => linkedList.insertAt(51, 2)).toThrow("out of range index");
+  });
+
+  test("remove method", () => {
+    const linkedList = new DoublyLinkedList<number>();
+    linkedList
+      .prepend(123)
+      .prepend(1)
+      .prepend(233)
+      .prepend(1323)
+      .prepend(9)
+      .prepend(2)
+      .prepend(3)
+      .prepend(4)
+      .prepend(65);
+
+    expect(linkedList.remove(1)).toBe(7);
+    expect(linkedList.getSize()).toBe(8);
     expect(linkedList.getContents()).toStrictEqual([
       65, 4, 3, 2, 9, 1323, 233, 123,
     ]);
   });
 
-  test("removeAt method", () => {
+  test("remove last item using remove method", () => {
     const linkedList = new DoublyLinkedList<number>();
-    linkedList.append(1).append(2).append(3).append(1323).append(9).removeAt(3);
+    linkedList
+      .prepend(123)
+      .prepend(1)
+      .prepend(233)
+      .prepend(1323)
+      .prepend(9)
+      .prepend(2)
+      .prepend(3)
+      .prepend(4)
+      .prepend(65);
 
-    expect(linkedList.getSize()).toBe(4);
-    expect(linkedList.getContents()).toStrictEqual([1, 2, 3, 9]);
+    expect(linkedList.remove(123)).toBe(8);
+    expect(linkedList.getSize()).toBe(8);
+    expect(linkedList.getContents()).toStrictEqual([
+      65, 4, 3, 2, 9, 1323, 233, 1,
+    ]);
   });
 
-  test("get method", () => {
+  test("remove method the last", () => {
     const linkedList = new DoublyLinkedList<number>();
-    linkedList.prepend(1).prepend(2).prepend(3).prepend(1323).prepend(9);
+    linkedList
+      .prepend(123)
+      .prepend(1)
+      .prepend(233)
+      .prepend(1323)
+      .prepend(9)
+      .prepend(2)
+      .prepend(3)
+      .prepend(4)
+      .prepend(65);
 
-    expect(linkedList.get(2)).toBe(3);
-    expect(linkedList.getContents()).toStrictEqual([9, 1323, 3, 2, 1]);
+    expect(linkedList.remove(1)).toBe(7);
+    expect(linkedList.getSize()).toBe(8);
+    expect(linkedList.getContents()).toStrictEqual([
+      65, 4, 3, 2, 9, 1323, 233, 123,
+    ]);
   });
+
+  test("remove method for only 1 item", () => {
+    const linkedList = new DoublyLinkedList<number>();
+    linkedList
+      .prepend(123);
+
+    expect(linkedList.remove(123)).toBe(0);
+    expect(linkedList.getSize()).toBe(0);
+    expect(linkedList.getContents()).toStrictEqual([]);
+  });
+
+
+  // test("removeAt method", () => {
+  //   const linkedList = new DoublyLinkedList<number>();
+  //   linkedList.append(1).append(2).append(3).append(1323).append(9).removeAt(3);
+
+  //   expect(linkedList.getSize()).toBe(4);
+  //   expect(linkedList.getContents()).toStrictEqual([1, 2, 3, 9]);
+  // });
+
+  // test("get method", () => {
+  //   const linkedList = new DoublyLinkedList<number>();
+  //   linkedList.prepend(1).prepend(2).prepend(3).prepend(1323).prepend(9);
+
+  //   expect(linkedList.get(2)).toBe(3);
+  //   expect(linkedList.getContents()).toStrictEqual([9, 1323, 3, 2, 1]);
+  // });
 });
