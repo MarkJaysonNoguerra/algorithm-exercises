947e630ed7baacc43467da14f96523c1709e5de2
mark jayson noguerra <noguerra9999@gmail.com>
2024-12-07 06:40:13 +0800

    reimplement sorting algorithm

--- src/algorithm/sorting/bubble-sort.ts
+++ src/algorithm/sorting/bubble-sort.ts
@@ -1,7 +1,15 @@
 // O(n squared)
 export const bubbleSort = (input: number[]): number[] => {
+
+    for (let i = 0; i < input.length; i++) {
+        for (let k = 0; k < input.length - i; k++) {
+            if (input[k] > input[k+1]) {
+                const temp = input[k];
+                input[k] = input[k+1];
+                input[k+1] = temp;
+            }
+        }
+    }
+
     return input;
 };
-
-
-// start implementing this on december 8 on wards.
--- src/algorithm/sorting/merge-sort.ts
+++ src/algorithm/sorting/merge-sort.ts
@@ -1,5 +1,35 @@
-export const mergeSort = (input: number[]): number[] => {
-  return input;
-};
+const combine = (left: number[], right: number[]): number[] => {
+  let leftIndex = 0;
+  let rightIndex = 0;
+  let result = [];
 
-// start implementing this on december 8 on wards.
+  for (; leftIndex < left.length && rightIndex < right.length;) {
+    if (left[leftIndex] < right[rightIndex]) {
+      result.push(left[leftIndex]);
+      leftIndex++;
+    } else {
+      result.push(right[rightIndex]);
+      rightIndex++;
+    }
+  }
+
+  for (; leftIndex < left.length; leftIndex++) {
+    result.push(left[leftIndex]);
+  }
+
+  for (; rightIndex < right.length; rightIndex++) {
+    result.push(right[rightIndex]);
+  }
+
+  return result;
+}
+
+
+export const mergeSort = (input: number[]): number[] => {
+  if (input.length === 1) {
+    return input;
+  }
+
+  const middle = Math.floor(input.length / 2);
+  return combine(mergeSort(input.slice(0, middle)), mergeSort(input.slice(middle)));
+};
